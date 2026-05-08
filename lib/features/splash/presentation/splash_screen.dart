9c5a737fa2c11b61079a19732f56381b8544085d
Naia Sila Azachra Firdaus <naiafirdaus8@gmail.com>
2026-05-08 11:39:31 +0700

    feat: add splash screen UI with Name and NIM

---
+++ lib/features/splash/presentation/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("UTD Store NAIA SILA AF", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            Text("NIM: 20123061"),
+          ],
+        ),
+      ),
+    );
+  }
+}
